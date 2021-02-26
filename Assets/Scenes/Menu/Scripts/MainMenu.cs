e7111c6e6aedcb90c735d2c91091454b319c21da
SofaLab24 <vincas.genys@gmail.com>
2021-02-26 21:09:15 +0200

    Create Chessboard

---
+++ Assets/Scenes/Menu/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
