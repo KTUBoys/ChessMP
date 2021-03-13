23f9c5495df535bfa029e9f70553ec82168b5c1b
Dominykas Ivanovas <dominykas.ivanovass@gmail.com>
2021-03-13 17:03:55 +0200

    Make pieces (Pawn) take on other pieces (pawn), comment code

---
+++ Assets/Scripts/GeneralMovement.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    // "Father" class for movement, has MonoBehaviour implementation
+    public class GeneralMovement : MonoBehaviour
+    {
+        public bool OutOfBorders(Vector3 curPos)
+        {
+            // Hard-coded values for borders
+            const float rightBorder = 1f;
+            const float leftBorder = -80f;
+            const float topBorder = 6.65f;
+            const float bottomBorder = -75f;
+
+            if (curPos.x >= rightBorder || curPos.x <= leftBorder) return true;
+
+            return curPos.z >= topBorder || curPos.z <= bottomBorder;
+        }
+
+        /// <summary>
+        /// Pretty complicated method
+        /// </summary>
+        /// <param name="curPos"> Current position </param>
+        /// <param name="defaultPos"> Default position </param>
+        /// <param name="rot"> Default rotation </param>
+        /// TODO rewrite for simplicity(?)
+        public void MoveToNearest(Vector3 curPos, Vector3 defaultPos, Quaternion rot)
+        {
+            // Take chessboard current stats
+            var cells = ChessBoard.BoardCoords;
+            var isFull = ChessBoard.IsFull;
+            var whoInCell = ChessBoard.WhoInCell;
+
+            // Get the current coords (where it was taken from) and put it into Point for comparison in for NN
+            var origin = new Point((int)curPos.x, (int)curPos.z);
+
+            // A neat LinQ expression for finding nearest neighbor
+            var neighbors = cells.Select(p => new { Point = p, Distance = CalculateTwoDistances(origin, p) })
+                .Where(pointAndDistance => pointAndDistance.Distance <= Math.Pow(7, 2))
+                .OrderBy(pointAndDistance => pointAndDistance.Distance)
+                .Select(pointAndDistance => pointAndDistance.Point)
+                .ToArray();
+
+            // Get indexes for default and nearest cell found to check if it's empty
+            var defaultCellIndex = Array.FindIndex(cells, point => point.X == (int)defaultPos.x && point.Y == (int)defaultPos.z);
+            var cellIndex = Array.FindIndex(cells, point => point.X == neighbors.First().X && point.Y == neighbors.First().Y);
+
+            // Variable for easier access
+            var whoInDefault = whoInCell[defaultCellIndex];
+
+            // Check if destination point is empty
+            if (isFull[cellIndex])
+            {
+                // Not empty, check if we are trying to make a move
+                var whoInDestination = whoInCell[cellIndex];
+                var whatColorO = whoInDefault.Split(' ')[0];
+                var whatColorD = whoInDestination.Split(' ')[0];
+                if (whatColorO == whatColorD)
+                {
+                    // Tried to take out our own piece. Move the piece back to its place
+                    gameObject.transform.SetPositionAndRotation(defaultPos, rot);
+                }
+                else
+                {
+                    // Good, the piece was actually our enemy. Let's take it down
+                    gameObject.transform.SetPositionAndRotation(new Vector3(neighbors.First().X, -0.9f, neighbors.First().Y), rot);
+                    isFull[cellIndex] = true;
+                    whoInCell[cellIndex] = whoInDefault;
+                    whoInCell[defaultCellIndex] = null;
+                    isFull[defaultCellIndex] = false;
+                    Destroy(GameObject.Find(whoInDestination));
+                }
+            }
+            else
+            {
+                // Empty destination, move successful
+                gameObject.transform.SetPositionAndRotation(new Vector3(neighbors.First().X, -0.9f, neighbors.First().Y), rot);
+                isFull[cellIndex] = true;
+                whoInCell[cellIndex] = whoInDefault;
+                whoInCell[defaultCellIndex] = null;
+                isFull[defaultCellIndex] = false;
+            }
+        }
+
+        private static float CalculateTwoDistances(Point originPoint, Point destinationPoint)
+        {
+            return (float)(Math.Pow(originPoint.X - destinationPoint.X, 2)
+                            + Math.Pow(originPoint.Y - destinationPoint.Y, 2));
+        }
+    }
+}
--- Assets/Scripts/PawnMovement.cs
+++ Assets/Scripts/PawnMovement.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Drawing;
-using System.Linq;
+﻿using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class PawnMovement : MonoBehaviour
+    /// <summary>
+    /// Pawn movement class inherited by GeneralMovement
+    /// </summary>
+    public class PawnMovement : GeneralMovement
     {
         private Vector3 pos;
         private Quaternion rot;
@@ -14,117 +15,51 @@ namespace Assets.Scripts
         private float mZCoord;          // mouse Z axis coords
         private Vector3 curPos;
 
+        [UsedImplicitly]    // for resharper
         void OnMouseDown()
         {
-            pos = gameObject.transform.position;
-            rot = gameObject.transform.rotation;
-            defaultPos = pos;
+            pos = gameObject.transform.position;        // Get current position
+            rot = gameObject.transform.rotation;        // Get current rotation
+            defaultPos = pos;                           // Put current position to default, to get default position later for any illegal moves
+
+
+            // Get current mouse position, make offset.
             gameObject.transform.SetPositionAndRotation(new Vector3(pos.x, pos.y + 1f, pos.z), rot);
             mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
             mOffset = gameObject.transform.position - GetMouseWorldPos();
+        }
+
+        [UsedImplicitly]    // For resharper
+        void OnMouseDrag()
+        {
+            curPos = transform.position = new Vector3(GetMouseWorldPos().x + mOffset.x, transform.position.y,
+                GetMouseWorldPos().z + mOffset.z);
+        }
+        
+        [UsedImplicitly]    // For reshaper
+        void OnMouseUp()
+        {
+            Move();
         }
 
         private Vector3 GetMouseWorldPos()
         {
             var mousePoint = Input.mousePosition;
 
-            mousePoint.z = mZCoord * 1.45f;
-            mousePoint.x *= 0.7f;
+            mousePoint.z = mZCoord * 1.45f;     // Multiplying by number due to mouse not actually following the piece correctly
+            mousePoint.x *= 0.7f;               // Doing the same to X axis, so the piece doesn't move too fast
 
             return Camera.main.ScreenToWorldPoint(mousePoint);
         }
 
-        void OnMouseDrag()
+        private void Move()
         {
-            curPos = transform.position = new Vector3(GetMouseWorldPos().x + mOffset.x, transform.position.y,
-                GetMouseWorldPos().z + mOffset.z);
-        }
-
-        void OnMouseUp()
-        {
-            if (OutOfBorders())
+            // Check for out of borders
+            if (OutOfBorders(curPos))
             {
                 gameObject.transform.SetPositionAndRotation(defaultPos, rot);
-            }
-            else MoveToNearest();
-
-        }
-
-        private bool OutOfBorders()
-        {
-            var rightBorder = 1f;
-            var leftBorder = -80f;
-            var topBorder = 6.65f;
-            var bottomBorder = -75f;
-
-            if (curPos.x >= rightBorder || curPos.x <= leftBorder)
-            {
-                return true;
-            }
-
-            if (curPos.z >= topBorder || curPos.z <= bottomBorder)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private void MoveToNearest()
-        {
-            pos = gameObject.transform.position;
-            var cells = ChessBoard.BoardCoords;
-            var isFull = ChessBoard.IsFull;
-            var whoInCell = ChessBoard.WhoInCell;
-            var origin = new Point((int) curPos.x, (int) curPos.z);
-            var neighbors = cells.Select(p => new {Point = p, Distance = CalculateTwoDistances(origin, p)})
-                .Where(pointAndDistance => pointAndDistance.Distance <= Math.Pow(7, 2))
-                .OrderBy(pointAndDistance => pointAndDistance.Distance)
-                .Select(pointAndDistance => pointAndDistance.Point)
-                .ToArray();
-
-            var defaultCellIndex = Array.FindIndex(cells, point => point.X == (int)defaultPos.x && point.Y == (int)defaultPos.z);
-            var cellIndex = Array.FindIndex(cells, point => point.X == neighbors.First().X && point.Y == neighbors.First().Y);
-            var whoInDefault = whoInCell[defaultCellIndex];
-
-            // Check if destination point is empty
-            if (isFull[cellIndex])
-            {
-                // Not empty, check if we are trying to make a move
-                var whoInDestination = whoInCell[cellIndex];
-                var whatColorO = whoInDefault.Split(' ')[0];
-                var whatColorD = whoInDestination.Split(' ')[0];
-                if (whatColorO == whatColorD)
-                {
-                    // Tried to take out our own piece. Move the piece back to its place
-                    gameObject.transform.SetPositionAndRotation(defaultPos, rot);
-                }
-                else
-                {
-                    // Good, the piece was actually our enemy. Let's take it down
-                    gameObject.transform.SetPositionAndRotation(new Vector3(neighbors.First().X, -0.9f, neighbors.First().Y), rot);
-                    isFull[cellIndex] = true;
-                    whoInCell[cellIndex] = whoInDefault;
-                    whoInCell[defaultCellIndex] = null;
-                    isFull[defaultCellIndex] = false;
-                    Destroy(GameObject.Find(whoInDestination));
-                }
-            }
-            else
-            {
-                // Empty destination, move successful
-                gameObject.transform.SetPositionAndRotation(new Vector3(neighbors.First().X, -0.9f, neighbors.First().Y), rot);
-                isFull[cellIndex] = true;
-                whoInCell[cellIndex] = whoInDefault;
-                whoInCell[defaultCellIndex] = null;
-                isFull[defaultCellIndex] = false;
-            }
-        }
-
-        private static float CalculateTwoDistances(Point originPoint, Point destinationPoint)
-        {
-            return (float) (Math.Pow(originPoint.X - destinationPoint.X, 2)
-                            + Math.Pow(originPoint.Y - destinationPoint.Y, 2));
+            }   // Else move to the nearest applicable cell.
+            else MoveToNearest(curPos, defaultPos, rot);
         }
     }
 }
