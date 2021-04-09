e0129f474d054d7bb23420ced8d8f9d8339314f9
Dominykas Ivanovas <58443450+CrabbyLT@users.noreply.github.com>
2021-04-09 18:20:32 +0300

    Remove conflict markers

--- Assets/Scripts/MovementSoundScript.cs
+++ Assets/Scripts/MovementSoundScript.cs
@@ -1,50 +1,25 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class MovementSoundScript : MonoBehaviour
     {
-        private AudioSource _pieceTakeSound;
-        private AudioSource _pieceMoveSound;
+        public AudioSource pieceTakeSound;
+        public AudioSource pieceMoveSound;
 
         private void Awake()
         {
-            _pieceTakeSound = GameObject.FindGameObjectWithTag("PieceTakeSound").GetComponent<AudioSource>();
-            _pieceMoveSound = GameObject.FindGameObjectWithTag("PieceMoveSound").GetComponent<AudioSource>();
+            pieceTakeSound = GameObject.FindGameObjectWithTag("PieceTakeSound").GetComponent<AudioSource>();
+            pieceMoveSound = GameObject.FindGameObjectWithTag("PieceMoveSound").GetComponent<AudioSource>();
         }
 
         public void MoveAPiece()
         {
-            _pieceMoveSound.Play();
+            pieceMoveSound.Play();
         }
         public void TakeAPiece()
         {
-            _pieceTakeSound.Play();
+            pieceTakeSound.Play();
         }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MovementSoundScript : MonoBehaviour
-{
-    private AudioSource pieceTakeSound;
-    private AudioSource pieceMoveSound;
-
-    private void Awake()
-    {
-        pieceTakeSound = GameObject.FindGameObjectWithTag("PieceTakeSound").GetComponent<AudioSource>();
-        pieceMoveSound = GameObject.FindGameObjectWithTag("PieceMoveSound").GetComponent<AudioSource>();
-    }
-
-    public void MoveAPiece()
-    {
-        pieceMoveSound.Play();
-    }
-    public void TakeAPiece()
-    {
-        pieceTakeSound.Play();
->>>>>>> origin/dev
     }
 }
