f5a1ea082e6c1e9e6fbc13eddb1042437b96e22d
Dominykas Plesevičius <dominykasplesevicius@gmail.com>
2021-04-18 15:06:15 +0300

    Create basic matchmaking by connecting to random servers

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private UIManager UIManager;
+
+    private void Update()
+    {
+        Debug.Log(PhotonNetwork.NetworkClientState.ToString());
+        UIManager.SetConnectionStatusText(PhotonNetwork.NetworkClientState.ToString());
+    }
+    public void OnPlayOnlineClick()
+    {
+        ConnectToServer();
+    }
+    private void ConnectToServer()
+    {
+        if (PhotonNetwork.IsConnected)
+            PhotonNetwork.JoinRandomRoom();
+        else
+            PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+}
